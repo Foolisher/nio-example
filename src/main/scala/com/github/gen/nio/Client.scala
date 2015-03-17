71f43c021745b90a86b513abc3d8a1a8c3f31797
wanggen <576192941@qq.com>
2015-03-17 17:30:33 +0800

    init

---
+++ src/main/scala/com/github/gen/nio/Client.scala
@@ -0,0 +1,11 @@
+package com.github.gen.nio
+
+/**
+ * <pre>
+ * 功能描述: 
+ * </pre>
+ * @author wanggen on 2015-03-17.
+ */
+class Client {
+
+}
---
+++ src/main/scala/com/github/gen/nio/Server.scala
@@ -0,0 +1,14 @@
+package com.github.gen.nio
+
+/**
+ * <pre>
+ * 功能描述: 
+ * </pre>
+ * @author wanggen on 2015-03-17.
+ */
+class Server {
+
+
+
+
+}
